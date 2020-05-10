5daa848e6ed28c712e72138481f3575daf12be8b
Daniel Watson <daniel.watson@nyu.edu>
2020-05-10 19:37:44 +0400

    Trying to fix structured bound evals

--- eval_gaussians.sh
+++ eval_gaussians.sh
@@ -14,9 +14,9 @@ eval_structured () {
     batch_sizes=( 10 100 1000 )
     for bs in "${batch_sizes[@]}"; do
         printf "$2\tno_divadd\t$bs\t"
-        rf estimate -s $1 -m models.$2 -d data_loaders.gaussian --batch_size $bs
+        rf estimate -s $1 -m models.$2 -d data_loaders.gaussian --batch_size $bs --gaussian $3
         printf "$2\tdivadd\t$bs\t"
-        rf estimate -s $1 -m models.$2 -d data_loaders.gaussian --batch_size $bs --div_add 1e-12
+        rf estimate -s $1 -m models.$2 -d data_loaders.gaussian --batch_size $bs --gaussian $3 --div_add 1e-12
     done
 }
 
@@ -27,9 +27,10 @@ for alpha_path in experiments/gaussians/*; do
     output_path=$alpha_path/estimates.tsv
     # echo "model\ttrial\tbatch_size\tscore" > $output_path
 
+    alpha="${alpha_path##*/alpha_}"
     baseline_models=("lb_nce" "ub_loo")
     for model in "${baseline_models[@]}"; do
-        eval_structured $alpha_path/$model $model >> $output_path
+        eval_structured $alpha_path/$model $model $alpha  >> $output_path
     done
 
     neural_models=("lb_dv" "lb_nwj" "ub_dv" "ub_nwj" "ub_variational")
@@ -42,7 +43,7 @@ for alpha_path in experiments/gaussians/*; do
                 continue
             fi
 
-            eval_neural $trial_path $model $trial >> $output_path
+            # eval_neural $trial_path $model $trial >> $output_path
         done
     done
 done
