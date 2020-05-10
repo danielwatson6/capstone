d4825eec0412e9d62fa03f984abce17623a75470
Daniel Watson <daniel.watson@nyu.edu>
2020-05-10 16:23:34 +0400

    Added redo NCE and LOO with fallback for nan

--- eval_gaussians.sh
+++ eval_gaussians.sh
@@ -7,7 +7,7 @@ eval_trial () {
     batch_sizes=( 10 100 1000 10000 )
     for bs in "${batch_sizes[@]}"; do
         printf "$2\t$3\t$bs\t"
-        rf estimate -s $1 -m models.$2 -d data_loaders.gaussian --batch_size $bs
+        rf estimate -s $1 -m models.$2 -d data_loaders.gaussian --batch_size $bs || rf estimate -s $1 -m models.$2 -d data_loaders.gaussian --batch_size $bs --div_add 1e-12
     done
 }
 
@@ -16,7 +16,7 @@ eval_trial () {
 # The files will contain the MI estimates for every trial and varying batch sizes.
 for alpha_path in experiments/gaussians/*; do
     output_path=$alpha_path/estimates.tsv
-    echo 'model\ttrial\tbatch_size\tscore' > $output_path
+    # echo 'model\ttrial\tbatch_size\tscore' > $output_path
 
     baseline_models=("lb_nce" "ub_loo")
     for model in "${baseline_models[@]}"; do
@@ -25,17 +25,17 @@ for alpha_path in experiments/gaussians/*; do
     done
 
     neural_models=("lb_dv" "lb_nwj" "ub_dv" "ub_nwj" "ub_variational")
-    for model in "${neural_models[@]}"; do
-        for trial_path in $alpha_path/$model/*; do
-            trial="${trial_path##*/}"
-            if [ "$trial" == ".kerastuner" ]; then
-                continue
-            elif [ "$trial" == "config.json" ]; then
-                continue
-            fi
+    # for model in "${neural_models[@]}"; do
+    #     for trial_path in $alpha_path/$model/*; do
+    #         trial="${trial_path##*/}"
+    #         if [ "$trial" == ".kerastuner" ]; then
+    #             continue
+    #         elif [ "$trial" == "config.json" ]; then
+    #             continue
+    #         fi
 
-            echo "evaluating $model"
-            eval_trial $trial_path $model $trial >> $output_path
-        done
-    done
+    #         echo "evaluating $model"
+    #         eval_trial $trial_path $model $trial >> $output_path
+    #     done
+    # done
 done
